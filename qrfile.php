a1d68e272849758b59f1a01479a6e927acbc24b5
Tore Fantoft Tuastad <tore@tuastad.com>
2021-05-03 19:41:19 +0200

    qrfile reads hasjh and gets base64 string

--- qrfile.php
+++ qrfile.php
@@ -3,12 +3,12 @@ include_once('./header.php');
 
 if (isset($_GET["file"])){
  $filehash=$_GET["file"];
- echo "filehhsh :" . $filehash;
- $sql = "SELECT * FROM images WHERE imgHash = '".$filehash . "';<br>";
- echo "SQL".$sql;
+ echo "filehash  :" . $filehash . '<br>';
+ $sql = "SELECT * FROM images WHERE imgHash = '" . $filehash . "';";
+ echo "<br>SQL: ".$sql . '<br>';
  $result = sql($sql);
- echo "<br> resultre:" . $result. '<br>';
- echo "<br>filename: " .$result["filename"];
+ //echo "<br> resultre:" . $result. '<br>';
+ //echo "<br>filename: " .$result["filename"];
  while ($row = $result->fetch_assoc()) {
     echo "<br> Filname: " . $row["filename"] . 
         "<br>content: " . $row["base64"] ;
